08a62eed03e30e80c8e43f6dea7d4846ae3a055c
demohari <kishore_koneru@yahoo.com>
2022-12-08 07:52:02 +0000

    added tests

--- template.py
+++ template.py
@@ -1,10 +1,10 @@
 import os
-import logging
 from pathlib import Path
+import logging
 
-logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+logging.basicConfig(level=logging.INFO, format="[%(asctime)s]: %(message)s: ")
 
-package_name = "DeepClassifier"
+package_name = "deepClassifier"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
@@ -15,18 +15,20 @@ list_of_files = [
     f"src/{package_name}/pipeline/__init__.py",
     f"src/{package_name}/entity/__init__.py",
     f"src/{package_name}/constants/__init__.py",
+    "tests/__init__.py",
+    "tests/unit/__init__.py",
+    "tests/integration/__init__.py",
     "configs/config.yaml",
     "dvc.yaml",
     "params.yaml",
     "init_setup.sh",
     "requirements.txt",
-    "requirements_dev"
+    "requirements_dev.txt",
     "setup.py",
     "setup.cfg",
     "pyproject.toml",
     "tox.ini",
-    "research/trial.ipynb"
- 
+    "research/trials.ipynb",
 ]
 
 for filepath in list_of_files:
@@ -34,12 +36,12 @@ for filepath in list_of_files:
     filedir, filename = os.path.split(filepath)
     if filedir != "":
         os.makedirs(filedir, exist_ok=True)
-        logging.info(f"Creating Directory: {filedir} for File: {filename}")
+        logging.info(f"Creating directory: {filedir} for file: {filename}")
 
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, "w") as f:
-            pass  # creates an empty file without overriding
-            logging.info(f"Creating Empty File: {filepath}")
+            pass  # create an empty file
+            logging.info(f"Creating empty file: {filepath}")
 
     else:
         logging.info(f"{filename} already exists")
