9f3d4976d2e2f81ca86f7581960f3bf1c4e4107b
demohari <kishore_koneru@yahoo.com>
2022-12-09 03:35:55 +0000

    deep imports

--- src/DeepClassifier/__init__.py
+++ src/DeepClassifier/__init__.py
@@ -1,18 +1,17 @@
 import os
 import sys
 import logging
-  
+
 logging_str = "[%(asctime)s: %(levelname)s: %(module)s]:%(message)s"
 logs_dir = "logs"
-log_filepath =os.path.join(logs_dir,"running_logs.log")
-os.makedirs(logs_dir,exist_ok=True)
+log_filepath = os.path.join(logs_dir, "running_logs.log")
+os.makedirs(logs_dir, exist_ok=True)
 
 logging.basicConfig(
-    level=logging.INFO, 
+    level=logging.INFO,
     format=logging_str,
-    handlers=[
-        logging.FileHandler(log_filepath),
-        logging.StreamHandler(sys.stdout)
+    handlers=[logging.FileHandler(log_filepath), 
+    # logging.StreamHandler(sys.stdout)],
     ])
 
 logger = logging.getLogger("deepClassifierLogger")
